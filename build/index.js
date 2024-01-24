1db4371370a7aebab562047cffe255ec1a841a1b
Claudinei Braga de Oliveira <82836309+claudinei05@users.noreply.github.com>
2024-01-24 12:17:03 -0300

    Delete build directory

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const typeorm_connection_1 = require("./main/database/typeorm.connection");
-const express_server_1 = require("./main/server/express.server");
-typeorm_connection_1.TypeormConnection.init().then(() => express_server_1.Server.run());
